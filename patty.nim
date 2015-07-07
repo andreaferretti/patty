4a8cd25ce045ad22852799746520d5388fde21ac
Andrea Ferretti <ferrettiandrea@gmail.com>
2015-07-07 19:58:00 +0200

    Cleanup

--- patty.nim
+++ patty.nim
@@ -159,82 +159,64 @@ macro variant*(e: expr, body: stmt): stmt {. immediate .} =
   when defined(pattydebug):
     echo toStrLit(result)
 
+
+proc matchBranch(n, sym: NimNode): NimNode {. compileTime .} =
+  n.expectKind(nnkCall)
+  n.expectMinLen(2)
+
+  let
+    obj = n[0]
+    statements = n[1]
+  statements.expectKind(nnkStmtList)
+
+  # We have a few cases for obj (the matching part)
+  # It could be
+  # - a matching clause like Circle(r: r)
+  # - a literal (not yet)
+  # - a bound or unbound variable (not yet)
+  obj.expectKind(nnkObjConstr)
+
+  # This is the new declaration section
+  var decl = newNimNode(nnkLetSection)
+  # These are the clauses for the bound variables
+  for c in tail(obj):
+    c.expectMinLen(2)
+    # ignore bindings to _
+    if $(c[1]) != "_":
+      decl.add(newIdentDefs(c[1], newEmptyNode(), newDotExpr(sym, c[0])))
+
+  # We transform the matching branch
+  # into a declaration of bound variables
+  # followed by the body, for instance
+  #
+  # let r = :tmp.r
+  # ...
+  var branchBody = newNimNode(nnkStmtList).add(decl)
+  for c in children(statements):
+    branchBody.add(c)
+  # This is the thing we dispatch on
+  let kindId = obj[0]
+  kindId.expectKind(nnkIdent)
+  result = newNimNode(nnkOfBranch).add(kindId, branchBody)
+
 macro match*(e: expr, body: stmt): stmt {. immediate .} =
+  body.expectKind(nnkStmtList)
   # A fresh symbol used to hold the evaluation of e
   let sym = genSym()
   # The node for the dispatch statement
   #
   # case :tmp.kind of:
   # ...
-  var dispatch = newNimNode(nnkCaseStmt)
-  dispatch.add(newDotExpr(sym, ident("kind")))
-
-  body.expectKind(nnkStmtList)
-  # The children of the body should look like calls
-  #
-  #   Circle(r: 5):
-  #     ...
-  #   Rectangle(w: 3, h: 4):
-  #     ...
+  var dispatch = newNimNode(nnkCaseStmt).add(newDotExpr(sym, ident("kind")))
   for child in children(body):
-    child.expectKind(nnkCall)
-    child.expectMinLen(2)
-
-    # For each child we had an object section
-    # for the matching part, and one or more
-    # statements to execute
-    let
-      obj = child[0]
-      statements = child[1]
-
-    statements.expectKind(nnkStmtList)
-
-    # We have a few cases for obj (the matchin part)
-    # It could be
-    # - a matching clause like Circle(r: r)
-    # - a literal
-    obj.expectKind(nnkObjConstr)
-
-    # This is the thing we dispatch on
-    let kindId = obj[0]
-    kindId.expectKind(nnkIdent)
-
-    # This is the new declaration section
-    var decl = newNimNode(nnkLetSection)
-    # These are the clauses for the bound variables
-    for c in tail(obj):
-      child.expectMinLen(2)
-      # ignore bindings to _
-      if $(c[1]) != "_":
-        decl.add(newIdentDefs(c[1], newEmptyNode(), newDotExpr(sym, c[0])))
-
-    # We transform the matching branch
-    # into a declaration of bound variables
-    # followed by the body, for instance
-    #
-    # let r = :tmp.r
-    # ...
-    var branchBody = newNimNode(nnkStmtList)
-    branchBody.add(decl)
-    for c in children(statements):
-      branchBody.add(c)
-
-    # This is the complete branch in the dispatch
-    # statement
-    var ofBranch = newNimNode(nnkOfBranch)
-    ofBranch.add(kindId)
-    ofBranch.add(branchBody)
-
-    dispatch.add(ofBranch)
+    dispatch.add(matchBranch(child, sym))
 
   # The whole thing is translated into a
   # declaration section where our temporary
   # symbol is assigned the value of e,
   # followed by the switch statement constructed
   # above
-  result = newNimNode(nnkStmtList)
-  result.add(newLetStmt(sym, e))
-  result.add(dispatch)
+  result = newStmtList(newLetStmt(sym, e), dispatch)
 
   when defined(pattydebug):
     echo toStrLit(result)
